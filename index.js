eb7a8ba7f165f4991be8dbdcb9936c15b5539da1
ALE <alierisir@gmail.com>
2023-05-23 16:36:59 +0300

    Loader Added

--- index.js
+++ index.js
@@ -1,151 +1,120 @@
-/* function getMath(arr) {
-const max = arr[0] > arr[1] ? arr[0] : arr[1]
-const min = arr[0] >= arr[1] ? arr[1] : arr[0]
-const mod = max % min
-const isEqual = max === min
-const sum = max+min
-const sub = max-min
-const pro = max*min
-const div = max/min
-const exp = max**min
-const obj = {
-    max,
-    min,
-    mod,
-    isEqual,
-    sum,
-    sub,
-    pro,
-    div,
-    exp,
-}
-return obj
-}
+import { Color } from "three";
+import { IfcViewerAPI } from "web-ifc-viewer";
 
-function generateRandomPairsArray(numPairs) {
-    const pairsArray = [];
-  
-    for (let i = 0; i < numPairs; i++) {
-      pairsArray.push([Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)]);
-    }
-  
-    return pairsArray;
-  }
-  
-let pairs = generateRandomPairsArray(1000);
+const container = document.getElementById("viewer-container");
+const viewer = new IfcViewerAPI({
+  container,
+  backgroundColor: new Color(0xffffff),
+});
 
-function cascade (arr) {
-    let objs =[]
-    for (item of arr) {
-        objs.push(getMath(item))
-    }
-    return objs
-    .filter(elem=>elem["min"]!==0)
-    .filter(elem=>elem["sum"]<80)  
-}
-
-myArray = cascade(pairs)
-
-function changeValue(obj){
-  let newObject = obj
-  newObject["sum"]="alierisir"
-  return newObject
-}
-
-let myObject =changeValue(myArray[0])
-
-console.log(myObject)
-
-
-function validateMail(mail){
-  const mailText= "@gmail.com"
-  const isNotMail = !mail.includes(mailText)
-  const isTooShort = mail.length < mailText.length+1
-  if (isNotMail || isTooShort) {
-    throw new Error ("Mail address is invalid")
-  }
-}
-
-function saveMail(mail) {
-  alert("Mail address is saved")
-}
-
-function main(){
-  const mail = prompt("Please enter your e-mail adress: ")
-  try {
-    validateMail(mail)
-    saveMail(mail)
-  } catch(error){
-    alert("Error occured:" + error)
-  } 
-}
-
-class Column {
-  constructor(guid){
-  this.guid=guid
-  this.height=undefined
-  this.length=undefined
-  this.width=undefined
-}
-  describe(string){
-    console.log(string,":", this[string])
-  }
-}
-
-const column001 = new Column("C001")
-
-column001.height=3
-column001.width=0.5
-column001.length=0.5
-
-class Plant {
-  constructor(name){
-    this.name=name
-    this.scent;
-    this.tast;
-  }
-  smell(){
-    const message=`You smelled the ${this.name} and it smelled ${this.scent}`
-    console.log(message)
-  }
-  taste(){
-    const message=`You tasted the ${this.name} and it tasted ${this.tast}`
-    console.log(message)
-  }
-}`and it tasted ${this.tast}`
-
-class Fruit extends Plant{
-  constructor(name) {
-  super(name);
-  this.scent="nice";
-  this.tast="sweet";
-  }
-}
-
-class Vegetable extends Plant{
-  constructor(name) {
-  super(name);
-  this.scent="good";
-  this.tast="salty";
-  }
-}
-const myFruit = new Fruit("elma")
-const myVeggie= new Vegetable("salatalık")
-
-*/
-
-import { Color } from 'three';
-import { IfcViewerAPI } from 'web-ifc-viewer';
-
-const container = document.getElementById('viewer-container');
-const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
+// Create grid and axes
 viewer.grid.setGrid();
 viewer.axes.setAxes();
 
-async function loadIfc(url) {
-    await viewer.IFC.setWasmPath("../../../");
-    const model = await viewer.IFC.loadIfcUrl(url);
-    viewer.shadowDropper.renderShadow(model.modelID);
+// Load IFC File
+const input = document.getElementById("file-input");
+
+input.addEventListener(
+  "change",
+
+  async (changed) => {
+    const file = changed.target.files[0];
+    const ifcURL = URL.createObjectURL(file);
+    const model = await viewer.IFC.loadIfcUrl(ifcURL);
+    //await viewer.shadowDropper.renderShadow(model.modelID);
+    viewer.context.renderer.postProduction.active = true
+    const ifcProject = await viewer.IFC.getSpatialStructure(model.modelID);
+    createTreeMenu(ifcProject); 
+  },
+
+  false
+);
+
+// Prepick and picking
+window.ondblclick = async () => {
+  await viewer.IFC.selector.pickIfcItem(true);
+  viewer.context.renderer.postProduction.active = true
+}
+window.onmousemove = async () => await viewer.IFC.selector.prePickIfcItem();
+
+// Tree view
+
+const toggler = document.getElementsByClassName("caret");
+for (let i = 0; i < toggler.length; i++) {
+    toggler[i].onclick = () => {
+        toggler[i].parentElement.querySelector(".nested").classList.toggle("active");
+        toggler[i].classList.toggle("caret-down");
+    }
 }
 
-loadIfc('./static/05.ifc');
+// Spatial tree menu
+
+function createTreeMenu(ifcProject) {
+    const root = document.getElementById("tree-root");
+    removeAllChildren(root);
+    const ifcProjectNode = createNestedChild(root, ifcProject);
+    ifcProject.children.forEach(child => {
+        constructTreeMenuNode(ifcProjectNode, child);
+    })
+}
+
+function nodeToString(node) {
+    return `${node.type} - ${node.expressID}`
+}
+
+function constructTreeMenuNode(parent, node) {
+    const children = node.children;
+    if (children.length === 0) {
+        createSimpleChild(parent, node);
+        return;
+    }
+    const nodeElement = createNestedChild(parent, node);
+    children.forEach(child => {
+        constructTreeMenuNode(nodeElement, child);
+    })
+}
+
+function createNestedChild(parent, node) {
+    const content = nodeToString(node);
+    const root = document.createElement('li');
+    createTitle(root, content);
+    const childrenContainer = document.createElement('ul');
+    childrenContainer.classList.add("nested");
+    root.appendChild(childrenContainer);
+    parent.appendChild(root);
+    return childrenContainer;
+}
+
+function createTitle(parent, content) {
+    const title = document.createElement("span");
+    title.classList.add("caret");
+    title.onclick = () => {
+        title.parentElement.querySelector(".nested").classList.toggle("active");
+        title.classList.toggle("caret-down");
+    }
+    title.textContent = content;
+    parent.appendChild(title);
+}
+
+function createSimpleChild(parent, node) {
+    const content = nodeToString(node);
+    const childNode = document.createElement('li');
+    childNode.classList.add('leaf-node');
+    childNode.textContent = content;
+    parent.appendChild(childNode);
+
+    childNode.onmouseenter = () => {
+        viewer.IFC.selector.prepickIfcItemsByID(0, [node.expressID]);
+    }
+
+    childNode.onclick = async () => {
+        viewer.IFC.selector.pickIfcItemsByID(0, [node.expressID],true);
+        console.log(node)
+    }
+}
+
+function removeAllChildren(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
